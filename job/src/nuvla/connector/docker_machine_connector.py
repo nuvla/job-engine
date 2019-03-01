8b620c0488d889e8a3083517adcdf2a5b85b5852
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-03-01 17:40:01 +0100

    add minimal lifecycle for docker-machine

---
+++ job/src/nuvla/connector/docker_machine_connector.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import logging
+import requests
+import machine as DockerMachine
+
+from .connector import Connector, should_connect
+
+import time
+from tempfile import NamedTemporaryFile
+import re
+from exceptions import Exception, RuntimeError
+
+
+class DockerMachineConnector(Connector):
+
+    def __init__(self, api_infrastructure_service, api_service_credential):
+        super(DockerMachineConnector, self).__init__(api_infrastructure_service, api_service_credential)
+
+        self.state = api_infrastructure_service.get("state")
+        self.driver_credential = api_service_credential
+        self.driver = api_infrastructure_service.get("cloud-service")
+
+        self.machine = DockerMachine.Machine()
+
+        # for now
+        self.machineBaseName = api_infrastructure_service.get("id")
+
+        self.multiplicity = api_infrastructure_service.get("multiplicity", 1)
+
+    @property
+    def connector_type(self):
+        return 'docker-machine'
+
+    def connect(self):
+        version = self.machine.version()
+        logging.info("Initializing a local docker-machine. Version: %s" % version)
+
+    def clear_connection(self):
+        # the container where this job run will be terminated, thus no cleanup needed
+        pass
+
+    def _get_full_url(self):
+        return self.machine.url(machine=self.machineBaseName)
+
+    @staticmethod
+    def validate_action(response):
+        """Takes the raw response from _start_container_in_docker
+        and checks whether the service creation request was successful or not"""
+        pass
+
+    def _vm_get_ip(self):
+        return self.machine.ip(machine=self.machineBaseName)
+
+    def _vm_get_id(self):
+        return self.machineBaseName
+
+    def _vm_get_state(self):
+        if self.machine.status(machine=self.machineBaseName):
+            return "Running"
+        else:
+            return "Stopped"
+
+    @should_connect
+    def start(self, api_deployment):
+        logging.info('start docker-machine')
+        pass
+
+    @should_connect
+    def stop(self, ids):
+        # remember to retrieve config and certs from service resource
+        self.machine.rm(machine=self.machineBaseName)
+        pass
+
+    @should_connect
+    def list(self):
+        self.machine.ls()
