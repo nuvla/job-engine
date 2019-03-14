35abf9775826780c3de51dde37698ad435e4075b
Cristovao Cordeiro <cristovao.cordeiro@sixsq.com>
2019-03-14 14:19:32 +0100

    Update executor.py
    always log exceptions

--- job/src/nuvla/job/executor.py
+++ job/src/nuvla/job/executor.py
@@ -63,9 +63,9 @@ class Executor(Base):
             except JobUpdateError as e:
                 logging.error('{} update error: {}'.format(job.id, str(e)))
             except Exception as _:
-                logging.error('Failed to process {}.'.format(job.id))
                 ex_type, ex_msg, ex_tb = sys.exc_info()
                 status_message = ''.join(traceback.format_exception(etype=ex_type, value=ex_msg, tb=ex_tb))
+                logging.error('Failed to process {}, with error:'.format(job.id, status_message))
                 job.update_job(state='FAILED', status_message=status_message)
             else:
                 job.update_job(state='SUCCESS', return_code=return_code)
