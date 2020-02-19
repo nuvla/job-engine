e5a3e512223869b96b196e02c7ead840b46427ad
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-02-19 14:50:30 +0100

    create nuvlabox jobs for reboot and check api

---
+++ code/src/nuvla/job/actions/nuvlabox_check_api.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import logging,os
+
+from ..actions import action
+from nuvla.connector import nuvlabox_connector as NB
+
+
+@action('check_nuvlabox_api')
+class NBRebootJob(object):
+
+    def __init__(self, _, job):
+        self.job = job
+        self.api = job.api
+
+    def check_api(self):
+        nuvlabox_id = self.job['target-resource']['href']
+
+        logging.info('Checking API for NuvlaBox {}.'.format(nuvlabox_id))
+        connector = NB.NuvlaBoxConnector(api=self.api, nuvlabox_id=nuvlabox_id, job=self.job)
+
+        # IMPORTANT BIT THAT MUST CHANGE FOR EVERY NUVLABOX API ACTION
+        r = connector.start(method='get')
+
+        return 0
+
+    def do_work(self):
+        return self.check_api()
