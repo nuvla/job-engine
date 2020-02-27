5da81fee2354d25f99d6f51c287444ee75b75188
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-02-27 13:24:47 +0100

    add infra check for verifying if swarm mode is enabled + optimize cimi calls-> pass infra when available

--- code/src/nuvla/connector/__init__.py
+++ code/src/nuvla/connector/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def connector_factory(connector_class, nuvla_api, credential_id):
+def connector_factory(connector_class, nuvla_api, credential_id, api_infrastructure_service=None):
     """
     Returns infrastructure connector instance. Connector to be used is derived
     from Nuvla via `credential_id`.
@@ -9,6 +9,7 @@ def connector_factory(connector_class, nuvla_api, credential_id):
     :param connector_class: Connector class to instanciate with
     :param nuvla_api: nuvla.api.Api, instance
     :param credential_id: str
+    :param api_infrastructure_service: if available, the corresponding infrastructure service
     :return: connector.Connector, instance
     """
 
@@ -20,8 +21,9 @@ def connector_factory(connector_class, nuvla_api, credential_id):
 
     api_credential = nuvla_api.get(credential_id).data
 
-    infrastructure_service_id = api_credential['parent']
+    if not api_infrastructure_service:
+        infrastructure_service_id = api_credential['parent']
 
-    api_infrastructure_service = nuvla_api.get(infrastructure_service_id).data
+        api_infrastructure_service = nuvla_api.get(infrastructure_service_id).data
 
     return connector_class.instantiate_from_cimi(api_infrastructure_service, api_credential)
