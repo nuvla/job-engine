0c23acd86cd153a38ca3d060e29cd895a2bf43f3
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-02-27 16:41:33 +0100

    remove check swarm job and re-use cred check instead

--- code/src/nuvla/job/actions/credential_check.py
+++ code/src/nuvla/job/actions/credential_check.py
@@ -22,10 +22,69 @@ class CredentialCheckCOEJob(object):
         self.job = job
         self.api = job.api
 
-    def check_coe_swarm(self, credential):
+    def check_coe_swarm(self, credential, infrastructure_service):
         connector = connector_factory(docker_cli_connector, self.api,
-                                      credential['id'])
-        info = connector.info()
+                                      credential['id'], api_infrastructure_service=infrastructure_service)
+
+        infra_online = infrastructure_service.get("online")
+        infra_swarm_enabled = infrastructure_service.get("swarm-enabled")
+        try:
+            info = connector.info()
+        except Exception as ex:
+            error_msg = ex.args[0]
+            if ("unable to resolve docker endpoint" in error_msg.lower()) or \
+                    ("invalid bind address format" in error_msg.lower()) or \
+                    ("cannot connect to the docker daemon at" in error_msg.lower()) or \
+                    ("is the docker daemon running" in error_msg.lower()):
+                # "unable to resolve docker endpoint: Invalid bind address format"
+                # it means that the infrastructure has a broken endpoint and will never work -> thus offline
+
+                # "Cannot connect to the Docker daemon at ... Is the docker daemon running?"
+                # it means that the infrastructure is reachable, but not the Docker API -> thus offline
+
+                if infra_online != False:
+                    self.api.edit(infrastructure_service.get("id"), {'online': False})
+            elif "remote error: tls" in error_msg.lower():
+                # "error during connect: Get <endpoint>/v1.40/info: remote error: tls: unknown certificate authority"
+                # in this case the infra is running, reachable, and Docker has replied. Simply the creds are not good
+                if infra_online != True:
+                    self.api.edit(infrastructure_service.get("id"), {'online': True})
+            else:
+                # other errors can simply mean that the server could not run the command, thus not being Docker related
+                # like:
+                # "open /tmp/key: no such file or directory" -> server side error
+                # "failed to retrieve context tls info: tls: failed to parse private key" -> broken user keys
+                # "no valid private key found" -> broken user keys format
+                # "failed to retrieve context tls info: tls: private key does not match public key" -> cred mismatch
+                # "check if the server supports the requested API version" -> client version incompatibility
+
+                # if we got here, is because the error is most likely not related with the infra, so just raise
+                pass
+
+            raise Exception(error_msg)
+
+        if infra_online != True:
+            self.api.edit(infrastructure_service.get("id"), {'online': True})
+
+        try:
+            node_id = info['Swarm']['NodeID']
+            managers = list(map(lambda x: x['NodeID'], info['Swarm']['RemoteManagers']))
+            if node_id in managers:
+                if infra_swarm_enabled != True:
+                    self.api.edit(infrastructure_service.get("id"), {'swarm-enabled': True})
+            else:
+                # The endpoint from infrastructure is not a manager
+                log.warning("Infrastructure {} does not have a Swarm manager".format(infrastructure_service.get("id")))
+                if infra_swarm_enabled != False:
+                    self.api.edit(infrastructure_service.get("id"), {'swarm-enabled': False})
+        except KeyError:
+            # then Swarm mode is not enabled
+            if infra_swarm_enabled != False:
+                self.api.edit(infrastructure_service.get("id"), {'swarm-enabled': False})
+        except:
+            # it's ok if we cannot infer the Swarm mode...so just move on
+            pass
+
         self.job.set_status_message(info)
 
     def check_coe_kubernetes(self, credential):
@@ -51,7 +110,7 @@ class CredentialCheckCOEJob(object):
 
         try:
             if infra_service['subtype'] == 'swarm':
-                self.check_coe_swarm(credential)
+                self.check_coe_swarm(credential, infra_service)
             elif infra_service['subtype'] == 'kubernetes':
                 self.check_coe_kubernetes(credential)
             self.update_credential_last_check(credential_id, 'VALID')
