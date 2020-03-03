d0d0a87e50bdaa7d86689db97b8f8f83a46a1ee9
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-03-03 08:15:24 +0100

    compare last version before check

--- code/src/nuvla/job/actions/application_compatibility_check.py
+++ code/src/nuvla/job/actions/application_compatibility_check.py
@@ -17,11 +17,19 @@ class ApplicationCompatibilityCheck(object):
         self.job = job
         self.api = job.api
 
-    @staticmethod
-    def check_config(compose_file):
+    def check_config(self, module):
         connector = docker_compose_cli_connector.DockerComposeCliConnector(endpoint="unix:///var/run/docker.sock")
 
-        return connector.check_app_compatibility(docker_compose=compose_file)
+        last_version = self.api.get(module['versions'][-1]['href']).data
+
+        before_last_version = self.api.get(module['versions'][-2]['href']).data if len(module['versions']) > 1 else {}
+
+        # check if the last commit has any changes in the compose file
+        # because if it doesn't, need the compatibility hasn't changed
+        if last_version.get('docker-compose') != before_last_version.get('docker-compose'):
+            return connector.check_app_compatibility(docker_compose=module['content']['docker-compose'])
+
+        return None
 
     def do_work(self):
         module_id = self.job['target-resource']['href']
@@ -33,17 +41,29 @@ class ApplicationCompatibilityCheck(object):
         self.job.set_progress(10)
 
         try:
-            compose_file = module['content']['docker-compose']
-            compatibility_mode = self.check_config(compose_file)
-            self.api.edit(module_id, {'compatibility': compatibility_mode})
-        except AssertionError as e:
-            log.error('{} failed in broken {}: {}'.format(self.job['action'], module, e))
-            self.job.set_status_message(e)
+            body = {
+                'versions': [{
+                    'author': module['versions'][-1]['author'],
+                    'commit': module['versions'][-1]['commit'] + " - auto compatibility check"
+                }]
+            }
+        except Exception as e:
+            self.job.set_status_message("Cannot parse last commit from {}: {}".format(module_id, e))
             return 1
-        except KeyError as e:
+
+        try:
+            compatibility_mode = self.check_config(module)
+            if compatibility_mode:
+                body['compatibility'] = compatibility_mode
+                self.api.edit(module_id, body)
+            else:
+                # no need to run compatibility check cause compose file hasn't changed
+                self.job.set_status_message("Nothing to do")
+        except Exception as e:
             log.exception('Cannot check compatibility for %s' % module_id)
             # default to swarm compatibility
-            self.api.edit(module_id, {'compatibility': 'swarm'})
+            body['compatibility'] = 'swarm'
+            self.api.edit(module_id, body)
             self.job.set_status_message(e)
             return 1
 
