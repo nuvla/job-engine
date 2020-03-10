f5be6007e05aa3bd5f7f8e5c75eeb781a9b5254a
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-03-10 14:50:24 +0100

    debug

--- code/src/nuvla/job/actions/deployment_log_fetch.py
+++ code/src/nuvla/job/actions/deployment_log_fetch.py
@@ -61,10 +61,10 @@ class DeploymentLogFetchJob(object):
 
             if Deployment.is_application(deployment):
                 if is_docker_compose:
-                    log.info(deployment_uuid)
+                    log.info(deployment_id)
                     log.info(service_name)
 
-                    docker_service_name = self.api_dpl.get_parameter(deployment_uuid,
+                    docker_service_name = self.api_dpl.get_parameter(deployment_id,
                                                                      service_name,
                                                                      service_name + '.service-id')
                 else:
