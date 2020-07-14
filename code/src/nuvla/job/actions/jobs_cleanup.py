1350a61ed7d50ce118c803bf30f56fefe62eb64d
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2020-07-14 12:21:03 +0200

    fix: delete_bulk call with positional arguments add static type def for nuvla field for signature hint in IDE

--- code/src/nuvla/job/actions/jobs_cleanup.py
+++ code/src/nuvla/job/actions/jobs_cleanup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ..actions import action
+from nuvla.api.api import Api as Nuvla
 
 import logging
 
@@ -10,14 +11,14 @@ class JobsCleanupJob(object):
 
     def __init__(self, _, job):
         self.job = job
-        self.nuvla = self.job.api
+        self.nuvla: Nuvla = self.job.api
 
     def cleanup_jobs(self):
         logging.info('Cleanup of completed jobs started.')
 
         days_back = 7
         filter = f"(state='SUCCESS' or state='FAILED') and created<'now-{days_back}d'"
-        ret = self.nuvla.delete_bulk(index='job', filter=filter)
+        ret = self.nuvla.delete_bulk('job', filter)
         if ret.data['timed_out']:
             msg = f'Cleanup of completed jobs have some failures: {ret.data}.'
             logging.warning(msg)
