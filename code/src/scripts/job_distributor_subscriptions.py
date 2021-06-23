1be71026c2e170621f681c19047acf98a28854cc
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2021-06-23 09:01:06 +0200

    initial impl

--- code/src/scripts/job_distributor_subscriptions.py
+++ code/src/scripts/job_distributor_subscriptions.py
@@ -6,6 +6,7 @@ from copy import copy
 from nuvla.job.base import main
 from nuvla.job.distributor import Distributor
 from nuvla.job.util import override
+from typing import Optional
 
 LAST = 10000
 
@@ -26,17 +27,21 @@ class SubscriptionsManager(Distributor):
         # no job generation
         # TODO: optimisation - consider caching the last check time and
         #  searching for the configs updated from e.g. (last check time - 15min).
-        for subs_conf in self._get_subscription_configs():
-            res_subs_ids = self._get_individual_subscriptions(subs_conf)
+        # all but 'event' resources
+        for subs_conf in self._get_subscription_configs(exclude_resource_kinds=['event']):
+            res_subs_ids = self._get_subscriptions(subs_conf)
             res_ids = self._resources_by_filter(subs_conf)
-            for v in res_ids.difference(res_subs_ids):
-                self._add_subscription(subs_conf, v)
-            for v in set(res_subs_ids) - res_ids:
-                self._delete_subscription(res_subs_ids[v])
+            for r_id in res_ids.difference(res_subs_ids):
+                self._add_subscription(subs_conf, r_id)
+            for r_id in set(res_subs_ids) - res_ids:
+                self._delete_subscription(res_subs_ids[r_id])
         return []
 
-    def _get_subscription_configs(self) -> dict:
-        subs_configs = self.api.search('subscription-config', last=LAST)
+    def _get_subscription_configs(self, exclude_resource_kinds=Optional[list]) -> dict:
+        flt = None
+        if exclude_resource_kinds:
+            flt = '(' + ' and '.join(map(lambda x: f"resource-kind!='{x}'", exclude_resource_kinds)) + ')'
+        subs_configs = self.api.search('subscription-config', filter=flt, last=LAST)
         logging.info(f'Subscription configs number: {subs_configs.count}')
         for r in subs_configs.resources:
             yield r.data
@@ -58,7 +63,7 @@ class SubscriptionsManager(Distributor):
         else:
             logging.debug(f'Deleted subscription {subs_id}')
 
-    def _resources_by_filter(self, subs_conf):
+    def _resources_by_filter(self, subs_conf) -> set:
         owner = subs_conf.get('acl').get('owners')[0]
         flt = f"acl/owners='{owner}' or acl/view-data='{owner}'"
         rf = subs_conf.get('resource-filter') or None
@@ -68,7 +73,7 @@ class SubscriptionsManager(Distributor):
                                     filter=flt, last=LAST)
         return set(map(lambda x: x.id, resources))
 
-    def _get_individual_subscriptions(self, subs_conf):
+    def _get_subscriptions(self, subs_conf) -> dict:
         filters = f"parent = '{subs_conf.get('id')}'"
         select = 'id,resource-id'
         subs = self.api.search('subscription', filter=filters, select=select, last=LAST)
