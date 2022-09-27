fbbf3eba1acd3a553206b6e33ad3851047479054
khaled basbous <khaled@sixsq.com>
2022-09-27 17:15:10 +0200

    start stop

--- code/src/nuvla/job/actions/deployment_set_stop.py
+++ code/src/nuvla/job/actions/deployment_set_stop.py
@@ -5,22 +5,22 @@ from ..actions import action
 from .utils.bulk_deployment import DeploymentBulkJob
 
 
-@action('start_deployment_set')
-class DeploymentSetStartJob(DeploymentBulkJob):
+@action('stop_deployment_set')
+class DeploymentSetStopJob(DeploymentBulkJob):
 
     def __init__(self, _, job):
         super().__init__(_, job)
         self.log_message = 'Deployment set'
 
     def action_deployment(self, deployment):
-        response = self.user_api.operation(deployment, 'start',
+        response = self.user_api.operation(deployment, 'stop',
                                            {'low-priority': True})
         return response.data['location']
 
     def do_work(self):
-        logging.info(f'Start deployment set start {self.job.id}')
-        result = self.run('start')
+        logging.info(f'Start deployment set stop {self.job.id}')
+        result = self.run('stop')
         deployment_set_id = self.job['target-resource']['href']
-        self.api.edit(deployment_set_id, {'state': 'STARTED'})
-        logging.info(f'End of deployment set start {self.job.id}')
+        self.api.edit(deployment_set_id, {'state': 'STOPPED'})
+        logging.info(f'End of deployment set stop {self.job.id}')
         return result
