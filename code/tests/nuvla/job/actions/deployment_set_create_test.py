c456ab2ac819eb5b8c9a23bf8f4efcc027b56da0
khaled basbous <khaled@sixsq.com>
2022-11-24 17:04:37 +0100

    fix code smell

--- code/tests/nuvla/job/actions/deployment_set_create_test.py
+++ code/tests/nuvla/job/actions/deployment_set_create_test.py
@@ -4,6 +4,9 @@ import unittest
 from nuvla.job.actions.deployment_set_create \
     import app_compatible_with_target, env_dict, coupons_dict
 
+module_a = 'module/a'
+module_b = 'module/b'
+
 
 class TestDeploymentSetCreate(unittest.TestCase):
 
@@ -15,25 +18,25 @@ class TestDeploymentSetCreate(unittest.TestCase):
 
     def test_coupon_dict(self):
         self.assertEqual({}, coupons_dict([]))
-        self.assertEqual({'module/a': 'a'}, coupons_dict([{'application': 'module/a',
-                                                           'code': 'a'}]))
-        self.assertEqual({'module/a': 'a',
-                          'module/b': 'b'}, coupons_dict([{'application': 'module/a', 'code': 'a'},
-                                                          {'application': 'module/b', 'code': 'b'}]))
+        self.assertEqual({module_a: 'a'}, coupons_dict([{'application': module_a,
+                                                         'code': 'a'}]))
+        self.assertEqual({module_a: 'a',
+                          module_b: 'b'}, coupons_dict([{'application': module_a, 'code': 'a'},
+                                                        {'application': module_b, 'code': 'b'}]))
 
     def test_env_dict(self):
         self.assertEqual({}, env_dict([]))
-        self.assertEqual({'module/a': {'name_a': 'a'}}, dict(env_dict([{'application': 'module/a',
-                                                                        'name': 'name_a',
-                                                                        'value': 'a'}])))
-        self.assertEqual({'module/a': {'name_a': 'a',
-                                       'name_c': 'c'},
-                          'module/b': {'name_b': 'b'}}, dict(env_dict([{'application': 'module/a',
-                                                                        'name': 'name_a',
-                                                                        'value': 'a'},
-                                                                       {'application': 'module/b',
-                                                                        'name': 'name_b',
-                                                                        'value': 'b'},
-                                                                       {'application': 'module/a',
-                                                                        'name': 'name_c',
-                                                                        'value': 'c'}])))
+        self.assertEqual({module_a: {'name_a': 'a'}}, dict(env_dict([{'application': module_a,
+                                                                      'name': 'name_a',
+                                                                      'value': 'a'}])))
+        self.assertEqual({module_a: {'name_a': 'a',
+                                     'name_c': 'c'},
+                          module_b: {'name_b': 'b'}}, dict(env_dict([{'application': module_a,
+                                                                      'name': 'name_a',
+                                                                      'value': 'a'},
+                                                                     {'application': module_b,
+                                                                      'name': 'name_b',
+                                                                      'value': 'b'},
+                                                                     {'application': module_a,
+                                                                      'name': 'name_c',
+                                                                      'value': 'c'}])))
