04415a11f11fa93cb34ed3a3d7c9388c81ab77bd
giovannibianco <john.white@sixsq.com>
2023-07-10 13:08:50 +0200

    extract the user home value. 1.

--- code/src/nuvla/job/actions/nuvlabox_add_ssh_key.py
+++ code/src/nuvla/job/actions/nuvlabox_add_ssh_key.py
@@ -38,7 +38,7 @@ class NBAddSSHKey(object):
             pubkey = self.job.context[credential_id]['public-key']
             if os.getenv('KUBERNETES_SERVICE_HOST'):
                 logging.info('We are using Kubernetes on nuvlabox ID : %s ',nuvlabox_id) # FIXME remove
-                self._add_ssh_key_k8s(pubkey)
+                self._add_ssh_key_k8s(pubkey) # FIXME we do also need to know the expected home directory on the host system.
             else:
                 connector = NB.NuvlaBox(api=self.api, nuvlabox_id=nuvlabox_id, job=self.job)
 
@@ -46,6 +46,7 @@ class NBAddSSHKey(object):
             ssh_keys = connector.nuvlabox.get('ssh-keys', [])
 
             if credential_id not in ssh_keys:
+                # FIXME actually maybe here we can test for Kubernetes?
                 r = connector.nuvlabox_manage_ssh_key('add-ssh-key', pubkey)
 
                 update_payload = ssh_keys + [credential_id]
@@ -64,6 +65,10 @@ class NBAddSSHKey(object):
     def _add_ssh_key_k8s(self, pubkey):
         logging.info('We must wait for the other pull request to be merged.') # FIXME
         connector = K8sSSHKey(self.job)
+        # FIXME ... get the home directory here?
+        nuvlabox_status = self.api.get("nuvlabox-status").data
+        user_home = nuvlabox_status.get('host-user-home')
+        logging.info('Extracted a user home value of : %s ',user_home)
         self.job.set_progress(10)
         connector.handleSSHKey()
         self.job.set_progress(90)
