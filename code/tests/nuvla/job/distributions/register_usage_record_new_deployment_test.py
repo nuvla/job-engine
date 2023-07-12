d3116d9c861557c1fbf4b420e291487a332f2c68
khaled basbous <khaled@sixsq.com>
2023-07-12 11:06:10 +0200

    minor

--- code/tests/nuvla/job/distributions/register_usage_record_new_deployment_test.py
+++ code/tests/nuvla/job/distributions/register_usage_record_new_deployment_test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 from nuvla.job.distribution import DistributionBase
 from nuvla.job.distributions.register_usage_record_new_deployment import \
     RegisterUsageRecordNewDeploymentJobsDistribution
+from nuvla.api.models import CimiResource
 
 
-class TestDeploymentStateJobsDistributor(unittest.TestCase):
+class TestRegisterUsageRecordNewDeploymentJobsDistribution(unittest.TestCase):
     def setUp(self):
         self.patcher = patch.object(DistributionBase, '_start_distribution')
         self.mock_object = self.patcher.start()
@@ -20,20 +21,24 @@ class TestDeploymentStateJobsDistributor(unittest.TestCase):
 
         self.assertEqual([], jd.unique_owners([]))
 
-        self.assertEqual(["user/a", "user/b"], jd.unique_owners(
-            [{"owner": "user/a"},
-             {"owner": "user/b"},
-             {"owner": "user/a"}]))
+        user_a = "user/a"
+        user_b = "user/b"
+        group_c = "group/c"
 
-        self.assertEqual(["user/a", "user/b", "group/c"], jd.unique_owners(
-            [{"owner": "user/a"},
-             {"owner": "user/b"},
-             {"owner": "user/a"},
-             {"owner": "group/c"}]))
+        self.assertEqual([user_a, user_b], jd.unique_owners(
+            [CimiResource({"owner": user_a}),
+             CimiResource({"owner": user_b}),
+             CimiResource({"owner": user_a})]))
 
-        self.assertEqual(["user/a", "user/b"], jd.unique_owners(
-            [{"owner": "user/a"},
-             {"owner": "user/b"},
-             {"owner": "user/a"},
-             {"owner": "group/c",
-              "module": {"acl": {"edit-data": ["group/c"]}}}]))
+        self.assertEqual([user_a, user_b, group_c], jd.unique_owners(
+            [CimiResource({"owner": user_a}),
+             CimiResource({"owner": user_b}),
+             CimiResource({"owner": user_a}),
+             CimiResource({"owner": group_c})]))
+
+        self.assertEqual([user_a, user_b], jd.unique_owners(
+            [CimiResource({"owner": user_a}),
+             CimiResource({"owner": user_b}),
+             CimiResource({"owner": user_a}),
+             CimiResource({"owner": group_c,
+                           "module": {"acl": {"edit-data": [group_c]}}})]))
