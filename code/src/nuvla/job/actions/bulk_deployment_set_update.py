18f94ef46dd41f103c31f1ea5aa901a3cd6ba58c
khaled basbous <khaled@sixsq.com>
2023-09-11 13:29:14 +0200

    bulk deployment set apply helper and do not call ok/nok on deployment set

--- code/src/nuvla/job/actions/bulk_deployment_set_update.py
+++ code/src/nuvla/job/actions/bulk_deployment_set_update.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from ..actions import action
-from .bulk_deployment_set_start import BulkDeploymentSetStartJob
+from .utils.bulk_deployment_set_apply import BulkDeploymentSetApply
 
 
 @action('bulk_deployment_set_update')
-class BulkDeploymentSetUpdateJob(BulkDeploymentSetStartJob):
+class BulkDeploymentSetUpdateJob(BulkDeploymentSetApply):
 
     def __init__(self, _, job):
         super().__init__(_, job)
+        self.action('update')
