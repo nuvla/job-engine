nuvla/job-engine

commit f757d275c2e0c35810798cc06577c1dac13396c0

docker_compose.py: give environment to all commands that might requires it. refactor connectors and deployment related job actions.

code/src/nuvla/connector/docker_stack.py
@@ -3,8 +3,11 @@ import json
 import logging
 from datetime import datetime
 from tempfile import TemporaryDirectory
-from .utils import execute_cmd, join_stderr_stdout, create_tmp_file, \
-    generate_registry_config, extract_host_from_url, LOCAL
+from .utils import (create_tmp_file,
+                    execute_cmd,
+                    generate_registry_config,
+                    join_stderr_stdout,
+                    LOCAL)
 from .connector import Connector, should_connect
 
 log = logging.getLogger('docker_stack')
@@ -60,7 +63,7 @@ class DockerStack(Connector):
     def start(self, **kwargs):
         # Mandatory kwargs
         docker_compose = kwargs['docker_compose']
-        stack_name = kwargs['stack_name']
+        stack_name = kwargs['name']
         env = kwargs['env']
         files = kwargs['files']
         registries_auth = kwargs['registries_auth']
@@ -98,7 +101,7 @@ class DockerStack(Connector):
     @should_connect
     def stop(self, **kwargs):
         # Mandatory kwargs
-        stack_name = kwargs['stack_name']
+        stack_name = kwargs['name']
 
         cmd = self.build_cmd_line(['stack', 'rm', stack_name])
         return join_stderr_stdout(execute_cmd(cmd))
@@ -185,8 +188,8 @@ class DockerStack(Connector):
         return services
 
     @should_connect
-    def stack_services(self, stack_name):
-        return self._stack_services(stack_name)
+    def get_services(self, name, **kwargs):
+        return self._stack_services(name)
 
     @should_connect
     def info(self):
@@ -196,18 +199,6 @@ class DockerStack(Connector):
         if len(server_errors) > 0:
             raise Exception(server_errors[0])
         return info
-
-    def extract_vm_id(self, vm):
-        pass
-
-    def extract_vm_ip(self, services):
-        return extract_host_from_url(self.endpoint)
-
-    def extract_vm_ports_mapping(self, vm):
-        pass
-
-    def extract_vm_state(self, vm):
-        pass
 
     @staticmethod
     def registry_login(**kwargs):

code/src/nuvla/connector/connector.py
@@ -40,6 +40,7 @@ class Connector(object):
     def connect(self):
         pass
 
+    @abstractmethod
     def clear_connection(self, connect_result):
         pass
 
@@ -63,17 +64,6 @@ class Connector(object):
         pass
 
     @abstractmethod
-    def extract_vm_id(self, vm):
-        pass
-
-    @abstractmethod
-    def extract_vm_ip(self, vm):
-        pass
-
-    @abstractmethod
-    def extract_vm_ports_mapping(self, vm):
-        pass
-
-    @abstractmethod
-    def extract_vm_state(self, vm):
+    @should_connect
+    def get_services(self, name, env, **kwargs):
         pass

code/src/nuvla/connector/docker_compose.py
@@ -10,6 +10,7 @@ from .connector import Connector, should_connect
 
 log = logging.getLogger('docker_compose')
 
+docker_compose_filename = 'docker-compose.yaml'
 
 def instantiate_from_cimi(api_infrastructure_service, api_credential):
     return DockerCompose(
@@ -65,18 +66,18 @@ class DockerCompose(Connector):
             return execute_cmd(cmd, **kwargs)
         except Exception as e:
             error = self.sanitize_command_output(str(e))
-            raise Exception(error)
+            raise RuntimeError(error)
 
     @should_connect
     def start(self, **kwargs):
         # Mandatory kwargs
         docker_compose = kwargs['docker_compose']
-        project_name = kwargs['stack_name']
+        project_name = kwargs['name']
         env = kwargs['env']
         registries_auth = kwargs['registries_auth']
 
         with TemporaryDirectory() as tmp_dir_name:
-            compose_file_path = tmp_dir_name + "/docker-compose.yaml"
+            compose_file_path = f'{tmp_dir_name}/{docker_compose_filename}'
             compose_file = open(compose_file_path, 'w')
             compose_file.write(docker_compose)
             compose_file.close()
@@ -113,25 +114,26 @@ class DockerCompose(Connector):
                     env=env,
                     timeout=int(env['DOCKER_CLIENT_TIMEOUT'])))
 
-            services = self._stack_services(project_name, compose_file_path)
+            services = self._get_services(project_name, compose_file_path, env)
 
             return result, services
 
     @should_connect
     def stop(self, **kwargs):
         # Mandatory kwargs
-        project_name = kwargs['stack_name']
+        project_name = kwargs['name']
         docker_compose = kwargs['docker_compose']
+        env = kwargs.get('env')
 
         with TemporaryDirectory() as tmp_dir_name:
-            compose_file_path = tmp_dir_name + "/docker-compose.yaml"
+            compose_file_path = f'{tmp_dir_name}/{docker_compose_filename}'
             compose_file = open(compose_file_path, 'w')
             compose_file.write(docker_compose)
             compose_file.close()
 
             cmd = self.build_cmd_line(
                 ['-p', project_name, '-f', compose_file_path, 'down'])
-            return join_stderr_stdout(self._execute_clean_command(cmd))
+            return join_stderr_stdout(self._execute_clean_command(cmd, env=env))
 
     update = start
 
@@ -140,17 +142,18 @@ class DockerCompose(Connector):
         pass
 
     @should_connect
-    def log(self, component: str, since: datetime, lines: int,
-            **kwargs) -> str:
+    def log(self, component: str, since: datetime, lines: int, **kwargs) -> str:
         deployment_uuid = kwargs['deployment_uuid']
         docker_compose = kwargs['docker_compose']
+        env = kwargs.get('env')
+
         with TemporaryDirectory() as tmp_dir_name:
-            compose_file_path = tmp_dir_name + "/docker-compose.yaml"
+            compose_file_path = f'{tmp_dir_name}/{docker_compose_filename}'
             compose_file = open(compose_file_path, 'w')
             compose_file.write(docker_compose)
             compose_file.close()
             service_id = self._get_service_id(deployment_uuid, component,
-                                              compose_file_path)
+                                              compose_file_path, env)
         if service_id:
             since_opt = ['--since', since.isoformat()] if since else []
             list_opts = ['-t', '--tail', str(lines)] + since_opt + [service_id]
@@ -166,11 +169,11 @@ class DockerCompose(Connector):
         except KeyError:
             return ''
 
-    def _get_service_id(self, project_name, service, docker_compose_path):
+    def _get_service_id(self, project_name, service, docker_compose_path, env):
         cmd = self.build_cmd_line(
             ['-p', project_name, '-f', docker_compose_path,
              'ps', '-q', service])
-        stdout = self._execute_clean_command(cmd).stdout
+        stdout = self._execute_clean_command(cmd, env=env).stdout
 
         return yaml.load(stdout, Loader=yaml.FullLoader)
 
@@ -181,15 +184,15 @@ class DockerCompose(Connector):
         except KeyError:
             return {}
 
-    def _get_container_inspect(self, service_id):
+    def _get_container_inspect(self, service_id, env):
         cmd = self.build_cmd_line(['inspect', service_id], binary='docker')
 
-        return json.loads(self._execute_clean_command(cmd).stdout)
+        return json.loads(self._execute_clean_command(cmd, env=env).stdout)
 
-    def _extract_service_info(self, project_name, service, docker_compose_path):
+    def _extract_service_info(self, project_name, service, docker_compose_path, env):
         service_id = self._get_service_id(project_name, service,
-                                          docker_compose_path)
-        inspection = self._get_container_inspect(service_id)
+                                          docker_compose_path, env)
+        inspection = self._get_container_inspect(service_id, env)
         service_info = {
             'image': self._get_image(inspection),
             'service-id': service_id,
@@ -220,26 +223,27 @@ class DockerCompose(Connector):
                       "InsecureRequestWarning" not in line]
         return '\n'.join(new_output)
 
-    def _stack_services(self, project_name, docker_compose_path):
+    def _get_services(self, project_name, docker_compose_path, env):
         cmd = self.build_cmd_line(
             ['-f', docker_compose_path, 'config', '--services'], local=True)
 
-        stdout = self._execute_clean_command(cmd).stdout
+        stdout = self._execute_clean_command(cmd, env=env).stdout
 
         services = [self._extract_service_info(project_name, service,
-                                               docker_compose_path)
+                                               docker_compose_path, env)
                     for service in stdout.splitlines()]
         return services
 
     @should_connect
-    def stack_services(self, stack_name, raw_compose_file):
+    def get_services(self, name, env, **kwargs):
+        compose_file_content = kwargs['compose_file']
         with TemporaryDirectory() as tmp_dir_name:
-            compose_file_path = tmp_dir_name + "/docker-compose.yaml"
+            compose_file_path = f'{tmp_dir_name}/{docker_compose_filename}'
             compose_file = open(compose_file_path, 'w')
-            compose_file.write(raw_compose_file)
+            compose_file.write(compose_file_content)
             compose_file.close()
 
-            return self._stack_services(stack_name, compose_file_path)
+            return self._get_services(name, compose_file_path, env)
 
     @should_connect
     def info(self):
@@ -251,18 +255,6 @@ class DockerCompose(Connector):
         if len(server_errors) > 0:
             raise Exception(server_errors[0])
         return info
-
-    def extract_vm_id(self, vm):
-        pass
-
-    def extract_vm_ip(self, services):
-        return extract_host_from_url(self.endpoint)
-
-    def extract_vm_ports_mapping(self, vm):
-        pass
-
-    def extract_vm_state(self, vm):
-        pass
 
     @staticmethod
     def registry_login(**kwargs):
@@ -288,7 +280,7 @@ class DockerCompose(Connector):
         env = kwargs['env']
 
         with TemporaryDirectory() as tmp_dir_name:
-            compose_file_path = tmp_dir_name + "/docker-compose.yaml"
+            compose_file_path = f'{tmp_dir_name}/{docker_compose_filename}'
             compose_file = open(compose_file_path, 'w')
             compose_file.write(docker_compose)
             compose_file.close()

code/src/nuvla/connector/docker_service.py
@@ -126,7 +126,7 @@ class DockerService(Connector):
         image = kwargs['image']
 
         # Optional kwargs
-        service_name = kwargs.get('service_name')
+        service_name = kwargs.get('name')
         env = kwargs.get('env')
         mounts_opt = kwargs.get('mounts_opt', [])
         ports_opt = kwargs.get('ports_opt', [])
@@ -374,15 +374,6 @@ class DockerService(Connector):
 
     def nodes_list_active(self):
         return self.nodes_list(availability='active')
-
-    def extract_vm_id(self, vm):
-        return vm['ID']
-
-    def extract_vm_ip(self, vm):
-        return extract_host_from_url(self.endpoint)
-
-    def extract_vm_state(self, vm):
-        return 'running'
 
     def extract_vm_ports_mapping(self, vm):
         published_ports_list = [":".join([str(pp.get("Protocol")),

code/src/nuvla/connector/kubernetes.py
@@ -6,8 +6,10 @@ import base64
 import logging
 import yaml
 from tempfile import TemporaryDirectory
-from .utils import execute_cmd, join_stderr_stdout, create_tmp_file, \
-    generate_registry_config, extract_host_from_url
+from .utils import (create_tmp_file,
+                    execute_cmd,
+                    generate_registry_config,
+                    join_stderr_stdout)
 from .connector import Connector, should_connect
 
 log = logging.getLogger('kubernetes')
@@ -65,7 +67,7 @@ class Kubernetes(Connector):
                + list_cmd
 
     @staticmethod
-    def _create_deployment_context(directory_path, stack_name, docker_compose,
+    def _create_deployment_context(directory_path, namespace, docker_compose,
                                    files, registries_auth):
         manifest_file_path = directory_path + '/manifest.yml'
         with open(manifest_file_path, 'w') as manifest_file:
@@ -75,18 +77,18 @@ class Kubernetes(Connector):
         with open(namespace_file_path, 'w') as namespace_file:
             namespace_data = {'apiVersion': 'v1',
                               'kind': 'Namespace',
-                              'metadata': {'name': stack_name}}
+                              'metadata': {'name': namespace}}
             yaml.safe_dump(namespace_data, namespace_file, allow_unicode=True)
 
-        kustomization_data = {'namespace': stack_name,
+        kustomization_data = {'namespace': namespace,
                               'resources': ['manifest.yml', 'namespace.yml']}
 
         if files:
             for file_info in files:
                 file_path = directory_path + "/" + file_info['file-name']
-                file = open(file_path, 'w')
-                file.write(file_info['file-content'])
-                file.close()
+                f = open(file_path, 'w')
+                f.write(file_info['file-content'])
+                f.close()
 
         if registries_auth:
             config = generate_registry_config(registries_auth)
@@ -117,20 +119,21 @@ class Kubernetes(Connector):
     def start(self, **kwargs):
         # Mandatory kwargs
         env = kwargs['env']
-        docker_compose = kwargs['docker_compose']
+        files = kwargs['files']
+        manifest = kwargs['docker_compose']
+        namespace = kwargs['name']
+        registries_auth = kwargs['registries_auth']
+
         envsubst_shell_format = ' '.join(['${}'.format(k) for k in env.keys()])
         cmd = ['envsubst', envsubst_shell_format]
-        docker_compose_env_subs = execute_cmd(cmd,
-                                              env=env,
-                                              input=docker_compose).stdout
-        registries_auth = kwargs['registries_auth']
-        stack_name = kwargs['stack_name']
-        files = kwargs['files']
+        manifest_env_subs = execute_cmd(cmd,
+                                        env=env,
+                                        input=manifest).stdout
 
         with TemporaryDirectory() as tmp_dir_name:
             Kubernetes._create_deployment_context(tmp_dir_name,
-                                                  stack_name,
-                                                  docker_compose_env_subs,
+                                                  namespace,
+                                                  manifest_env_subs,
                                                   files,
                                                   registries_auth)
 
@@ -138,7 +141,7 @@ class Kubernetes(Connector):
 
             result = join_stderr_stdout(execute_cmd(cmd_deploy))
 
-            services = self._stack_services(stack_name)
+            services = self._get_services(namespace)
 
             return result, services
 
@@ -146,7 +149,7 @@ class Kubernetes(Connector):
 
     @should_connect
     def stop(self, **kwargs):
-        namespace = kwargs['stack_name']
+        namespace = kwargs['name']
 
         cmd_stop = self.build_cmd_line(['delete', 'namespace', namespace])
 
@@ -459,15 +462,15 @@ class Kubernetes(Connector):
                         external_port)
         return service_info
 
-    def _stack_services(self, stack_name):
+    def _get_services(self, namespace):
         cmd_services = self.build_cmd_line(['get', 'services', '--namespace',
-                                            stack_name, '-o', 'json'])
+                                            namespace, '-o', 'json'])
         kube_services = json.loads(execute_cmd(cmd_services).stdout).get(
             'items', [])
 
         cmd_deployments = self.build_cmd_line(
             ['get', 'deployments', '--namespace',
-             stack_name, '-o', 'json'])
+             namespace, '-o', 'json'])
         kube_deployments = json.loads(execute_cmd(cmd_deployments).stdout).get(
             'items', [])
 
@@ -483,18 +486,5 @@ class Kubernetes(Connector):
         return json.loads(version)
 
     @should_connect
-    def stack_services(self, stack_name):
-        return self._stack_services(stack_name)
-
-    def extract_vm_id(self, vm):
-        pass
-
-    def extract_vm_ip(self, services):
-        return extract_host_from_url(self.endpoint)
-
-    def extract_vm_ports_mapping(self, vm):
-        pass
-
-    def extract_vm_state(self, vm):
-        pass
-
+    def get_services(self, name, env, **kwargs):
+        return self._get_services(name)

code/src/nuvla/connector/nuvlabox.py
@@ -156,18 +156,6 @@ class NuvlaBox(Connector):
 
         return True
 
-    def extract_vm_id(self, vm):
-        pass
-
-    def extract_vm_ip(self, services):
-        pass
-
-    def extract_vm_ports_mapping(self, vm):
-        pass
-
-    def extract_vm_state(self, vm):
-        pass
-
     def get_installer_image_names(self, nuvlaedge_version):
         repo = 'installer'
         old = ('nuvlabox', 'master')

code/src/nuvla/job/actions/deployment_log_fetch.py
@@ -4,8 +4,11 @@ import logging
 from datetime import datetime
 from nuvla.api.resources import Deployment
 from ..actions import action
-from .utils.deployment_utils import get_connector_class, get_connector_name, \
-    initialize_connector, docker_stack
+from .utils.deployment_utils import (get_connector_class,
+                                     get_connector_name,
+                                     initialize_connector,
+                                     docker_stack,
+                                     get_env)
 from .utils.resource_log_fetch import ResourceLogFetchJob
 
 action_name = 'fetch_deployment_log'
@@ -45,7 +48,9 @@ class DeploymentLogFetchJob(ResourceLogFetchJob):
         return self.connector.log(
             component, since, lines,
             deployment_uuid=Deployment.uuid(self.deployment),
-            docker_compose=module_content['docker-compose'])
+            docker_compose=module_content['docker-compose'],
+            env=get_env(self.deployment.data)
+        )
 
     def get_docker_stack_log(self, component, since, lines):
         if self.connector_name == 'docker_service':

code/src/nuvla/job/actions/deployment_start.py
@@ -66,7 +66,7 @@ class DeploymentStartJob(DeploymentBase):
         desired = 1
 
         deployment_parameters = (
-            (DeploymentParameter.SERVICE_ID, connector.extract_vm_id(service)),
+            (DeploymentParameter.SERVICE_ID, service['ID']),
             (DeploymentParameter.REPLICAS_DESIRED, str(desired)),
             (DeploymentParameter.REPLICAS_RUNNING, '0'),
             (DeploymentParameter.CURRENT_DESIRED, ''),
@@ -97,13 +97,11 @@ class DeploymentStartJob(DeploymentBase):
         connector_class  = get_connector_class(connector_name)
         connector        = initialize_connector(connector_class, self.job, deployment)
         module_content   = Deployment.module_content(deployment)
-        deployment_owner = Deployment.owner(deployment)
-        deployment_id    = Deployment.id(deployment)
         registries_auth  = self.private_registries_auth(deployment)
 
         result, services = connector.start(
+            name=Deployment.uuid(deployment),
             docker_compose=module_content['docker-compose'],
-            stack_name=Deployment.uuid(deployment),
             env=get_env(deployment),
             files=module_content.get('files'),
             registries_auth=registries_auth)

commit 5dd4094da85ec48498bf993577c8a8bdbbcbf124

docker_stack: fix bug introduced in f757d27
docker_stack.py: fix bug introduced in commit f757d275c2e0c35810798cc06577c1dac13396c0

code/src/nuvla/connector/docker_stack.py
@@ -188,7 +188,7 @@ class DockerStack(Connector):
         return services
 
     @should_connect
-    def get_services(self, name, **kwargs):
+    def get_services(self, name, env, **kwargs):
         return self._stack_services(name)
 
     @should_connect