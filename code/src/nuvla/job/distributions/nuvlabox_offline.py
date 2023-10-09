961cc4c9b51b8bdf4044086a4ffde41f34167fc6
Khaled Basbous <khaled@sixsq.com>
2023-10-09 09:26:16 +0200

    nuvlabox offline distributor - Replace nuvlabox status offline distributor (#288)

--- code/src/nuvla/job/distributions/nuvlabox_offline.py
+++ code/src/nuvla/job/distributions/nuvlabox_offline.py
@@ -6,34 +6,35 @@ from ..distributions import distribution
 from ..distribution import DistributionBase
 
 
-@distribution('update_nuvlabox_online')
-class NuvlaBoxStatusOfflineDistribution(DistributionBase):
-    DISTRIBUTION_NAME = 'update_nuvlabox_online'
+@distribution('nuvlabox_offline')
+class NuvlaBoxOfflineDistribution(DistributionBase):
+    DISTRIBUTION_NAME = 'nuvlabox_offline'
 
     def __init__(self, distributor):
-        super(NuvlaBoxStatusOfflineDistribution, self).__init__(
+        super(NuvlaBoxOfflineDistribution, self).__init__(
             self.DISTRIBUTION_NAME, distributor)
         self.collect_interval = 30
         self._start_distribution()
 
     def collect_offline(self):
-        filters = f"online = true and next-heartbeat < 'now'"
-        select = 'id'
+        filters = "online = true and next-heartbeat < 'now'"
+        select = 'id, parent'
         last = 10000
         offline = self.distributor.api.search('nuvlabox-status', filter=filters,
                                               select=select, last=last)
         logging.info(f'Nuvlabox offline: {offline.count}')
-        return offline.resources
+        return [status.data.get('parent') for status in offline.resources]
 
-    def set_status_offline(self, nb_status_id):
+    def set_offline(self, nb_id):
         try:
-            self.distributor.api.edit(nb_status_id, {'online': False})
+            nuvlabox =self.distributor.api.get(nb_id)
+            self.distributor.api.operation(nuvlabox, 'set-offline')
         except Exception as ex:
-            logging.error(f'Failed edit {nb_status_id} to set offline : {ex}')
+            logging.error(f'Failed edit {nb_id} to set offline : {ex}')
 
     @override
     def job_generator(self):
         # we don't generate a job because it's a simple edit of nuvlabox status
-        for nuvlabox_status in self.collect_offline():
-            self.set_status_offline(nuvlabox_status.id)
+        for nuvlabox in self.collect_offline():
+            self.set_offline(nuvlabox)
         return []
