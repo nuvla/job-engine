738afceb875de846ef9794724a1add31d3aaa356
Nacho <nacho@sixsq.com>
2024-03-25 16:03:48 +0100

    remove unnecessary directory from local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -3,13 +3,12 @@
 IMAGE_ORG=${1:-local}
 IMAGE_REPO=${2:-job}
 
-mkdir -p $TEMP_OUTPUT_DIR || true
-
 job_engine_version=$(poetry version -s)
 IMAGE_TAG_NAME=$IMAGE_ORG/$IMAGE_REPO:$job_engine_version
 
 echo "Building job-engine version $job_engine_version to $IMAGE_TAG_NAME"
 
+poetry lock
 poetry build -f wheel --no-interaction
 # Export plugin will be removed from default poetry, we need to install it manually from pip
 pip install poetry poetry-plugin-export
