d8e7faa72d9217e4250516bf2b853ccfa4890200
ignacio-penas <ignacio.penas4b@gmail.com>
2024-05-08 15:42:13 +0200

    relocate pip install

--- local-build.sh
+++ local-build.sh
@@ -3,6 +3,9 @@
 IMAGE_ORG=${1:-local}
 IMAGE_REPO=${2:-job}
 
+# Export plugin will be removed from default poetry, we need to install it manually from pip
+pip install poetry poetry-plugin-export
+
 export job_engine_version=$(poetry version -s)
 export IMAGE_TAG_NAME=$IMAGE_ORG/$IMAGE_REPO:$job_engine_version
 
@@ -10,8 +13,6 @@ echo "Building job-engine version $job_engine_version to $IMAGE_TAG_NAME"
 
 poetry lock
 poetry build -f wheel --no-interaction
-# Export plugin will be removed from default poetry, we need to install it manually from pip
-pip install poetry poetry-plugin-export
 
 poetry export -f requirements.txt -o dist/requirements.txt --without-hashes --without-urls --with server
 
