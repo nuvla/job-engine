caca9c622e7a7f82da3739f09b57568063ba9dbf
ignacio-penas <ignacio.penas4b@gmail.com>
2024-10-25 11:39:50 +0200

    fix bug

--- nuvla/job_engine/job/base.py
+++ nuvla/job_engine/job/base.py
@@ -158,7 +158,6 @@ class Base(object):
             os.mkdir(cookie_dir, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
 
         with open(cookie_file, "w") as f:
-            MozillaCookieJar()
             f.write(cookies)
 
     def _init_nuvla_api(self):
