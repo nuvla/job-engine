9ed235d5e3d9a68db9d1074df81b254c27afe22b
khaled basbous <khaled@sixsq.com>
2025-04-04 16:03:18 +0200

    fix(distribution.py): Avoid stopping distribution on first exception fix(distribution.py): Sleep take into account execution time

--- nuvla/job_engine/job/distribution.py
+++ nuvla/job_engine/job/distribution.py
@@ -26,14 +26,16 @@ class DistributionBase():
         logging.info(f'I am {self.distributor.name} and I have been elected '
                      f'to distribute "{self.distribution_name}" jobs every {sleep_time}s')
         while not self.distributor.stop_event.is_set():
-            try:
-                for cimi_job in self.job_generator():
+            now = time.time()
+            for cimi_job in self.job_generator():
+                try:
                     logging.info(f'Distribute job: {cimi_job}')
                     self.distributor.api.add('job', cimi_job)
-                time.sleep(sleep_time)
-            except Exception as ex:
-                logging.error(f'Failed to distribute job {self.distribution_name}: {ex}')
-                time.sleep(5)
+                except Exception as ex:
+                    logging.error(f'Failed to distribute job {self.distribution_name} - {cimi_job}: {ex}')
+                    time.sleep(5)
+            te = time.time() - now
+            time.sleep(max(sleep_time - te, 0))
 
     def _start_distribution(self):
         election = self.distributor.kz.Election(f'/election/{self.distribution_name}', self.distributor.name)
