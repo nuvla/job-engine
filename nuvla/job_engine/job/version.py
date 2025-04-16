a7f8251af6c45dc55ca4b895e137f2c0dc234891
Khaled Basbous <khaled@sixsq.com>
2025-04-16 13:23:19 +0200

    simpler version check (#436)

--- nuvla/job_engine/job/version.py
+++ nuvla/job_engine/job/version.py
@@ -7,6 +7,8 @@ logger = logging.getLogger(__name__)
 
 VERSION_TRIM_RE = re.compile(r'\.?[^.0-9]+[0-9]*$')
 
+_SUPPORTED_VERSION = 2
+
 def _get_version_from_env():
     return os.getenv('JOB_ENGINE_VERSION')
 
@@ -17,43 +19,22 @@ def _get_version_from_package():
         logger.warning('Cannot retrieve Job-engine version')
         return None
 
-def _version_to_tuple(engine_version_str: str) -> tuple:
-    ver_ = list(map(int, VERSION_TRIM_RE.sub('', engine_version_str).split('.')))
-    if len(ver_) < 2:
-        return ver_[0], 0, 0
-    return tuple(ver_)
-
-def _compute_version_min(engine_version_tuple: tuple):
-    if engine_version_tuple[0] < 2:
-        return 0, 0, 1
-    else:
-        return 1, 0, 0
-
-class JobVersionBiggerThanEngine(Exception):
+class JobVersionNotYetSupported(Exception):
     pass
 
 class JobVersionIsNoMoreSupported(Exception):
     pass
 
 class Version(object):
-    engine_version_str = _get_version_from_env() or _get_version_from_package()
-    engine_version = None
-    engine_version_min_support = None
-    engine_version_min_support_str = None
-    if engine_version_str:
-        engine_version = _version_to_tuple(engine_version_str)
-        engine_version_min_support = _compute_version_min(engine_version)
-        engine_version_min_support_str = '.'.join(map(str, engine_version_min_support))
+    engine_version = _get_version_from_env() or _get_version_from_package()
 
     @classmethod
     def job_version_check(cls, job_version_str: str):
-        job_version = _version_to_tuple(job_version_str)
-
-        if cls.engine_version is None:
-            logger.warning("Job-Engine version is not known. Ignoring version checks")
-            return
-
-        if job_version < cls.engine_version_min_support:
+        try:
+            job_version = int(job_version_str)
+        except ValueError:
+            job_version = 0
+        if job_version < _SUPPORTED_VERSION:
             raise JobVersionIsNoMoreSupported()
-        elif job_version > cls.engine_version:
-            raise JobVersionBiggerThanEngine()
+        elif job_version > _SUPPORTED_VERSION:
+            raise JobVersionNotYetSupported()
--- tests/job/version_test.py
+++ tests/job/version_test.py
@@ -8,61 +8,45 @@ class VersionTestCase(unittest.TestCase):
 
     def test_env_version_not_set(self):
         importlib.reload(version)
-        self.assertIsNone(version.Version.engine_version_str)
         self.assertIsNone(version.Version.engine_version)
-        self.assertIsNone(version.Version.engine_version_min_support)
-        self.assertIsNone(version.Version.engine_version_min_support_str)
+        self.assertIsNone(version.Version.engine_version)
+        self.assertIsNone(version.Version.job_version_check('2'))
+        with self.assertRaises(version.JobVersionIsNoMoreSupported):
+            version.Version.job_version_check('1')
+        with self.assertRaises(version.JobVersionIsNoMoreSupported):
+            version.Version.job_version_check('0')
 
     @patch.dict(os.environ, {'JOB_ENGINE_VERSION': '2.8.7'})
     def test_env_version_only_set(self):
         importlib.reload(version)
-        self.assertEqual(version.Version.engine_version_str, '2.8.7')
-        self.assertEqual(version.Version.engine_version, (2, 8, 7))
-        self.assertEqual(version.Version.engine_version_min_support, (1, 0, 0))
-        self.assertEqual(version.Version.engine_version_min_support_str, '1.0.0')
+        self.assertEqual(version.Version.engine_version, '2.8.7')
 
     @patch('importlib_metadata.version', return_value='2.1.3')
     def test_env_version_not_set_package_set(self, _mock):
         importlib.reload(version)
-        self.assertEqual(version.Version.engine_version_str, '2.1.3')
-        self.assertEqual(version.Version.engine_version, (2, 1, 3))
-        self.assertEqual(version.Version.engine_version_min_support, (1, 0, 0))
+        self.assertEqual(version.Version.engine_version, '2.1.3')
 
     @patch.dict(os.environ, {'JOB_ENGINE_VERSION': '2.8.7'})
     @patch('importlib_metadata.version', return_value='2.1.3')
     def test_env_version_preferred_over_package(self, _mock):
         importlib.reload(version)
-        self.assertEqual(version.Version.engine_version_str, '2.8.7')
-        self.assertEqual(version.Version.engine_version, (2, 8, 7))
-        self.assertEqual(version.Version.engine_version_min_support, (1, 0, 0))
+        self.assertEqual(version.Version.engine_version, '2.8.7')
 
     @patch.dict(os.environ, {})
     @patch('importlib_metadata.version', return_value='2.1.3')
     def test_env_version_empty_package_version_set(self, _mock):
         importlib.reload(version)
-        self.assertEqual(version.Version.engine_version_str, '2.1.3')
-        self.assertEqual(version.Version.engine_version, (2, 1, 3))
-        self.assertEqual(version.Version.engine_version_min_support, (1, 0, 0))
+        self.assertEqual(version.Version.engine_version, '2.1.3')
 
     @patch.dict(os.environ, {'JOB_ENGINE_VERSION': '2.8.7'})
     def test_check_version(self):
         importlib.reload(version)
-        self.assertIsNone(version.Version.job_version_check('2.8.7'))
-        self.assertIsNone(version.Version.job_version_check('1.0.1'))
-        self.assertIsNone(version.Version.job_version_check('1.0.1.dev'))
-        self.assertIsNone(version.Version.job_version_check('1.0.1-dev'))
-        self.assertIsNone(version.Version.job_version_check('1'))
+        self.assertIsNone(version.Version.job_version_check('2'))
         with self.assertRaises(version.JobVersionIsNoMoreSupported):
-            version.Version.job_version_check('0.0.1')
+            version.Version.job_version_check('1')
         with self.assertRaises(version.JobVersionIsNoMoreSupported):
-            version.Version.job_version_check('0.1.1')
-        with self.assertRaises(version.JobVersionBiggerThanEngine):
-            version.Version.job_version_check('2.8.8')
-
-    def test_check_engine_version_not_set(self):
-        importlib.reload(version)
-        # assert that check version does nothing when no engine version was set
-        self.assertIsNone(version.Version.engine_version_str)
-        self.assertIsNone(version.Version.job_version_check('2.8.7'))
-        self.assertIsNone(version.Version.job_version_check('1.0.1'))
-        self.assertIsNone(version.Version.job_version_check('0.0.1'))
+            version.Version.job_version_check('0')
+        with self.assertRaises(version.JobVersionNotYetSupported):
+            version.Version.job_version_check('11')
+        with self.assertRaises(version.JobVersionNotYetSupported):
+            version.Version.job_version_check('3')
